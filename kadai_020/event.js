ced1883acf0992be5c8ca471d756ab08f1c92bbd
masayuki97 <162100110+masayuki97@users.noreply.github.com>
2024-04-25 14:16:29 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+const btnclick=document.getElementById('btn');
+btnclick.addEventListener('click',()=>{
+    const myText=document.getElementById('text');
+    myText.textContent='ボタンをクリックしました';    
+});
+
+
+
+
